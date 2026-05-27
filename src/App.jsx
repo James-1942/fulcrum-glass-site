ea651623bed49bd914542cf50b22a29b8cc81711
James-1942 <jamescornelio93@yahoo.com>
2026-05-27 19:58:52 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+
 import HomePage from "./pages/HomePage";
+import ShowerGlass from "./pages/ShowerGlass";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
+
+      <Route
+        path="/services/shower-glass"
+        element={<ShowerGlass />}
+      />
     </Routes>
   );
 }
