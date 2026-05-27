47a40e1ad3a7200f1a3a907b97308ffe7bfad58a
James-1942 <jamescornelio93@yahoo.com>
2026-05-27 19:46:00 -0400

    Create HomePage.jsx

---
+++ pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { ArrowRight } from "lucide-react";
+import "../App.css";
+import FulcrumHeader from "../FulcrumHeader";
+
+export default function HomePage() {
+  return (
+    <main className="site">
+
+      ALL YOUR CURRENT JSX HERE
+
+    </main>
+  );
+}
